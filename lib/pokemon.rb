5491b043a3d6801dd75abb3476172ed26cba05e4
Jeremy Emata <jeremy.emata@gmail.com>
2018-11-06 03:04:48 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -15,4 +15,8 @@ class Pokemon
     database_connection.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)",name, type)
   end 
 
+  def self.find(id)
+     @db.execute("SELECT FROM pokemon(name) VALUES (?)", )
+  end 
+
 end
